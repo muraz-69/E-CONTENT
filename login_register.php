fe5f4678ad690bf86b0319bd87bb7942168be056
hunter <hunter@kali.hunter.com>
2025-03-05 10:10:13 +0300

    blog web

---
+++ login_register.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+require_once 'config.php';
+
+if (isset($_POST['register'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $role = $_POST['role'];
+
+    $checkEmail = $conn->query("SELECT email FROM user WHERE email = '$email'");
+    if ($checkEmail->num_rows > 0) {
+        $_SESSION['register_error'] = 'Email is already registered!';
+        $_SESSION['active_form'] = 'register';
+} else {
+    $conn->query("INSERT INTO user (name, email, password, role) VALUES ('$name', '$email', '$password', '$role')");
+}
+
+header("Location: register.php");
+exit();
+  
+}
---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-content</title>
+    <link rel="stylesheet" href="register.css">
+</head>
+<body>
+    <div class="container">
+        <div class="form-box active" id="login-form">
+            <form action="login_register.php" method="post">
+                <h2>Login</h2>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit" name="login">Login</button>
+                <p>Don't have an account?<a href="#" onclick="showForm('register-form')">Register</a></p>
+            </form>
+        </div>
+
+        <div class="form-box" id="register-form">
+            <form action="login_register.php" method="post">
+                <h2>Register</h2>
+                <input type="text" name="name" placeholder="Name" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <select name="role" required>
+                    <option value="">--Select Role--</option>
+                    <option value="">User</option>
+                    <option value="">Admin</option>
+                </select>
+                <button type="submit" name="register">Register</button>
+                <p>Already have an account?<a href="#" onclick="showForm('login-form')">Login</a></p>
+            </form>
+        </div>
+    </div>
+ <script src="script.js"></script>   
+</body>
+</html>
